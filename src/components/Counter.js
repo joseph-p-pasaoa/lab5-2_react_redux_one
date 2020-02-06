3b0c6ee62eef8832e9ea33215d5d42a65179f11e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-06 09:27:41 -0500

    0.2, add increment (ifEven, ifOdd, Async) buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
+const Counter = ({ 
+  value,
+  onIncrement,
+  onDecrement,
+  onIncrementEven,
+  onIncrementOdd,
+  onIncrementAsync
+}) => (
   <div>
     <p>value: {value}</p>
-    <p>
+    <div>
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
-    </p>
+      <p>---</p>
+      <button onClick={onIncrementEven}>+ (only if even)</button>
+      <button onClick={onIncrementOdd}>+ (only if odd)</button>
+      <button onClick={onIncrementAsync}>+ (with 1s delay)</button>
+    </div>
   </div>
 );
 
