387e02d6f9a4242de510a860a64073eb44e3a498
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-06 09:37:46 -0500

    0.6, add increment (ifEven, ifOdd, Async) cases to count reducer

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,12 +1,36 @@
-import { INCREMENT, DECREMENT } from "../actions/actionTypes";
+import {
+  INCREMENT,
+  DECREMENT,
+  INCREMENT_IF_EVEN,
+  INCREMENT_IF_ODD,
+  INCREMENT_ASYNC
+} from "../actions/actionTypes";
 
 export default (state = 0, action) => {
+  let newState = state;
   switch (action.type) {
     case INCREMENT:
-      return state + 1;
+      newState += 1;
+      break;
     case DECREMENT:
-      return state - 1;
+      newState -= 1;
+      break;
+    case INCREMENT_IF_EVEN:
+      if (newState % 2 === 0) {
+        newState += 1;
+      }
+      break;
+    case INCREMENT_IF_ODD:
+      if (newState % 2 === 1) {
+        newState += 1;
+      }
+      break;
+    case INCREMENT_ASYNC:
+      console.log("IAsync")
+      setTimeout(newState += 1, 1000);
+      break;
     default:
-      return state;
+      return newState;
   }
+  return newState;
 };
